02396450d81858fd9a31ace3f6b81e3f7cc72748
codeconcept <contact@codeconcept.fr>
2020-03-06 11:55:09 +0100

    Save all answers to the server

--- src/cards/cards.controller.ts
+++ src/cards/cards.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { CardsService } from './cards.service';
 import { Card } from 'src/interfaces/card.interface';
+import { SaveAnswersDto } from './dto/save-answers.dto';
 
 @Controller('cards')
 export class CardsController {
@@ -10,5 +11,12 @@ export class CardsController {
     async getCards(): Promise<Card[]> {
         const cards = await this.cardsService.getCards();
         return cards;
+    }
+
+    @Post()
+    async saveAnswers(@Body() saveAnswersDto: SaveAnswersDto) {
+        const result = await this.cardsService.saveAnswers(saveAnswersDto);
+        console.log('saveAnswers result', result);
+        return result;
     }
 }
--- src/cards/cards.service.ts
+++ src/cards/cards.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { Card } from 'src/interfaces/card.interface';
+import { Answer } from 'src/interfaces/answer.interface';
 
 @Injectable()
 export class CardsService {
+    answers: Answer[] = [];
+
     getCards():Promise<Card[]> {
         const cards = [
             {id: 1, question: 'What is the language of the web?', answer: 'JavaScript'},
-            {id: 2, question: 'What is the the coolest job?', answer: 'Programmer'}
+            {id: 2, question: 'What is the the coolest job?', answer: 'Programmer'},
+            {id: 3, question: 'What is an interface?', answer: 'A contract'},
+            {id: 4, question: 'Is push() mutating an array?', answer: 'Unfortunatly yes'},
         ];
         return new Promise(resolve => {
             resolve(cards);
         })
+    }
+
+    saveAnswers(answers): Promise<{ msg: string, date: string }> {
+        return new Promise(resolve => {
+            this.answers = answers;
+            resolve({ msg: 'answers saved', date: new Date().toISOString() })
+        });
     }
 }
